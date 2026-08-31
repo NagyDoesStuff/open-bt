c99f26c2729ea29a32098f204c8db8e3aa202984
NagyDoesStuff <nagamer2021@gmail.com>
2026-08-31 15:06:02 +0200

    Added the Chomper and melee part animation support.

--- classes/parts/MeleePart.gd
+++ classes/parts/MeleePart.gd
@@ -3,6 +3,7 @@ class_name MeleePart
 
 @onready var melee_area: Area2D = $Area2D
 
+@export_group("Stats")
 @export var cooldown: float = 0.5
 @export var dmg_info: Dictionary = {
 	"type": "punch",
@@ -10,6 +11,11 @@ class_name MeleePart
 	"knk": 25.0,
 	"duration": 0.0
 }
+@export var instakill_class: int = 0
+
+@export_group("Visual")
+@export var attack_animation: String = "atk"
+@export var animation_player: AnimationPlayer
 
 var can_hit: bool = true
 
@@ -18,7 +24,14 @@ func _subready() -> void:
 		melee_area.area_entered.connect(hit)
 
 func hit(area: Area2D) -> void: 
-	if area is Cluster and user.team != area.team:
+	if area is Cluster and user.team != area.team and can_hit:
+		if area != GlobalClass.player_cluster and area.cluster_class <= instakill_class:
+			area.kill()
+		else:
+			area.recieve_hit(dmg_info, global_rotation)
+		
+		if animation_player:
+			animation_player.play(attack_animation)
+		
 		can_hit = false
-		area.recieve_hit(dmg_info, global_rotation)
 		get_tree().create_timer(cooldown).timeout.connect(set.bind("can_hit", true))
